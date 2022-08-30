84a091763f5d69d4353ef2acab762a0aec73e5ea
njoohyungs <joohyunggame@gmail.com>
2022-08-30 12:21:35 -0300

    First Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'cd36632f-2cc1-41d3-b3b0-6a09be163fd8';
+export const MODAL_CLOSE_SEC = 2.5;
